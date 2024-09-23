4edfcc8b8a599c1c5fd65a4a04b004dcc27cbfe0
chayon1001 <baust.cse.200201001@gmail.com>
2024-09-23 12:09:28 +0600

    clicking the donation button will hide the history data and display the donatation data and also clicking the history button will hide the donation data and display the history data added

---
+++ js/donation.js
@@ -0,0 +1,7 @@
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('history-data').classList.add('hidden');
+
+    document.getElementById('donation-data').classList.remove('hidden');
+
+    
+})
---
+++ js/history.js
@@ -0,0 +1,5 @@
+document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('donation-data').classList.add('hidden');
+   
+    document.getElementById('history-data').classList.remove('hidden')
+})
