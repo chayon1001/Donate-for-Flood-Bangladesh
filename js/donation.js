0f3b18b56a4335f36f0ceb146f8ef7e98c47c0ae
chayon1001 <baust.cse.200201001@gmail.com>
2024-09-24 09:27:22 +0600

    added the change the button color to indicate when its active

--- js/donation.js
+++ js/donation.js
@@ -3,5 +3,11 @@ document.getElementById('donation-btn').addEventListener('click', function(){
 
     document.getElementById('donation-data').classList.remove('hidden');
 
+
+    document.getElementById('donation-btn').classList.add('bg-primary','text-slate-900', 'font-semibold');
+
+    document.getElementById('history-btn').classList.remove('bg-primary','text-slate-900', 'font-semibold');
+
+
     
 })
--- js/history.js
+++ js/history.js
@@ -5,6 +5,10 @@ document.getElementById('history-btn').addEventListener('click', function(){
 
    
     
-    
+    document.getElementById('donation-btn').classList.remove('bg-primary','text-slate-900', 'font-semibold');
+
+    document.getElementById('donation-btn').classList.add('border-2', 'bg-gray-100');
+
+    document.getElementById('history-btn').classList.add('bg-primary','text-slate-900', 'font-semibold' );
 
 })
